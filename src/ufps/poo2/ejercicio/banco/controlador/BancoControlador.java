40315007b887d373cc6552003c410614fc296cdd
HP <HP@DESKTOP-KE10MH7>
2024-09-09 15:39:02 -0500

    hice todo el bancoControlador con metodos como: abrirCuenta, cenclarAbrirCuenta, aplicarAccion, cancelarAplicarAccion y enviarCorreos

--- src/ufps/poo2/ejercicio/banco/controlador/BancoControlador.java
+++ src/ufps/poo2/ejercicio/banco/controlador/BancoControlador.java
@@ -4,10 +4,69 @@
  */
 package ufps.poo2.ejercicio.banco.controlador;
 
+import ufps.poo2.ejercicio.banco.modelo.Account;
+import ufps.poo2.ejercicio.banco.modelo.Bank;
+import ufps.poo2.ejercicio.banco.vista.BancoVista;
+
 /**
  *
  * @author Boris Perez
  */
 public class BancoControlador {
+    
+    private BancoVista frame;
+    private Bank banco;
+
+    public BancoControlador(BancoVista frame) {
+        this.frame = frame;
+        this.banco = new Bank();
+    }
+
+    public void abrirCuenta() {
+        char tipo = frame.getRbAhorros().isSelected() ? 'A' : 'C';
+        int accnum = Integer.parseInt(frame.getTxtNumeroCuenta().getText());
+        double saldo = Double.parseDouble(frame.getTxtSaldoCuenta().getText());
+        banco.openAccount(tipo, accnum);
+        Account cuenta = banco.buscarCuenta(accnum);
+        banco.depositarCuenta(cuenta, saldo);
+        if(tipo=='A') frame.getTaMensajes().append("Se abrió cuenta de Ahorros num: "+accnum+" con saldo: "+saldo+"\n");
+        else frame.getTaMensajes().append("Se abrió cuenta Corriengte num: "+accnum+" con saldo: "+saldo+"\n");
+        
+    }
+
+    public void cancelarAbrirCuenta() {
+        frame.getTxtNumeroCuenta().setText("");
+        frame.getTxtSaldoCuenta().setText("");
+        frame.getRbAhorros().setSelected(false);
+        frame.getRbCorriente().setSelected(false);        
+    }
+
+    public void aplicarAccion() {
+        int accnum = Integer.parseInt(frame.getTxtNumeroCuentaAcciones().getText());
+        double valor = Double.parseDouble(frame.getTxtValor().getText());
+        if (frame.getRbDividendos().isSelected()){
+            banco.payDividend(accnum, valor);
+            frame.getTaMensajes().append("Pago de dividendos Cuenta num: "+accnum+" por: $"+valor+"\n");
+        } else {
+            banco.withdrawAccount(accnum, valor);
+            frame.getTaMensajes().append("Retiro a Cuenta num: "+accnum+" por: $"+valor+"\n");
+        }
+    }
+
+    public void cancelarAccion() {
+        frame.getTxtNumeroCuentaAcciones().setText("");
+        frame.getTxtValor().setText("");
+        frame.getRbDividendos().setSelected(false);
+        frame.getRbRetirar().setSelected(false);
+    }
+
+    public void enviarCorreos() {
+        frame.getTaMensajes().append(banco.sendLetterToOverdraftAccounts());
+    }
+
+    
+    
+    
+    
     
 }
